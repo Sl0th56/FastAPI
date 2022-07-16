f93ef79b2223dff674ebd728ef0e7e4a1c9d03a6
Sloth <rudo24@yandex.ru>
2022-07-16 11:44:08 +0500

    fixed

--- task/settings.py
+++ task/settings.py
@@ -1,17 +1,16 @@
 import os
 
 from pydantic import BaseSettings
-from starlette.config import Config
 from pathlib import Path
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 root_dir = f'{Path(__file__).parents[1]}/'
 path = os.path.join(dir_path, '.env')
-config = Config(f'{path}')
+DB_NAME = f'fastapi_app.db'
 
 class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     server_port: int = 8000
-    database_url = f'sqlite:///{root_dir}' + config('DB_NAME', cast=str)
+    database_url = f'sqlite:///{root_dir}' + DB_NAME
 
 settings = Settings()
